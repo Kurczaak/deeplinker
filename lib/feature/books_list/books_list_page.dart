cc7eabb8eccda450c353b76ed2ca0aae4ba0ceb6
Michał Kurczak <kurczakmichal97@gmail.com>
2024-08-10 08:00:28 +0200

    feat: add basic book list page

--- lib/feature/books_list/books_list_page.dart
+++ lib/feature/books_list/books_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:deeplinker/model/book.dart';
+import 'package:deeplinker/router/router.gr.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -7,6 +9,25 @@ class BooksListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: booksMocks.length,
+        itemBuilder: (context, index) {
+          final book = booksMocks[index];
+          return ListTile(
+            title: Text(book.title),
+            subtitle: Text(book.author),
+            onTap: () {
+              context.router.push(
+                BookDetailsRoute(
+                  bookId: book.id,
+                  book: book,
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
