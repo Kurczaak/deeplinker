258b87cab247369a06f99c72da1d5924026d3fdc
Michał Kurczak <kurczakmichal97@gmail.com>
2024-08-10 08:22:37 +0200

    chore: rename books shell page

--- lib/feature/books_list/book_list_shell_page.dart
+++
@@ -1,6 +0,0 @@
-import 'package:auto_route/auto_route.dart';
-
-@RoutePage()
-class BookListShellPage extends AutoRouter {
-  const BookListShellPage({super.key});
-}
---
+++ lib/feature/books_list/books_shell_page.dart
@@ -0,0 +1,6 @@
+import 'package:auto_route/auto_route.dart';
+
+@RoutePage()
+class BooksShellPage extends AutoRouter {
+  const BooksShellPage({super.key});
+}
