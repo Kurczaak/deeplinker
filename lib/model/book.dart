a50acdf796200578edbae104dd837cc57ebe96d8
Michał Kurczak <kurczakmichal97@gmail.com>
2024-08-10 08:22:50 +0200

    feat: Update book authors in book.dart

--- lib/model/book.dart
+++ lib/model/book.dart
@@ -25,35 +25,35 @@ List<Book> booksMocks = [
   Book(
     id: 2,
     title: 'To Kill a Mockingbird',
-    author: authorsMocks[2].name,
+    author: authorsMocks[1].name,
     description:
         'To Kill a Mockingbird is a novel by the American author Harper Lee. It was published in 1960 and was instantly successful. In the United States, it is widely read in high schools and middle schools. To Kill a Mockingbird has become a classic of modern American literature, winning the Pulitzer Prize.',
   ),
   Book(
     id: 3,
     title: '1984',
-    author: authorsMocks[3].name,
+    author: authorsMocks[2].name,
     description:
         '1984 is a dystopian social science fiction novel by English novelist George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell\'s ninth and final book completed in his lifetime. Thematically, Nineteen Eighty-Four centres on the consequences of totalitarianism, mass surveillance, and repressive regimentation of persons and behaviours within society.',
   ),
   Book(
     id: 4,
     title: 'The Catcher in the Rye',
-    author: authorsMocks[4].name,
+    author: authorsMocks[3].name,
     description:
         'The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951. It was originally intended for adults but is often read by adolescents for its themes of angst, alienation, and as a critique on superficiality in society.',
   ),
   Book(
     id: 5,
     title: 'Lord of the Flies',
-    author: authorsMocks[5].name,
+    author: authorsMocks[4].name,
     description:
         'Lord of the Flies is a 1954 novel by Nobel Prize-winning British author William Golding. The book focuses on a group of British',
   ),
   Book(
     id: 6,
     title: 'The Grapes of Wrath',
-    author: authorsMocks[6].name,
+    author: authorsMocks[5].name,
     description:
         'The Grapes of Wrath is an American realist novel written by John Steinbeck and published in 1939. The book won the National Book Award and Pulitzer Prize for fiction, and it was cited prominently when Steinbeck was awarded the Nobel Prize in 1962.',
   ),
