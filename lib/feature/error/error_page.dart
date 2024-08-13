467ef8bd351760daa11e5fd3c1f1dc8cc8ee8f8c
Michał Kurczak <kurczakmichal97@gmail.com>
2024-08-13 12:00:53 +0200

    feat: add error page

---
+++ lib/feature/error/error_page.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Error'),
+    );
+  }
+}
